c7d45e16d267356350aa39a412b92b4b15247c47
Edvard A. M. Haaland <fishurban@gmail.com>
2017-05-05 04:56:41 +0200

    Add files via upload

---
+++ names.js
@@ -0,0 +1,4 @@
+var elfNames = {elfMale, elfFemale, elfSurname};
+var elfMale = ["Adran", "Aelar", "Aramil", "Arannis", "Aust"];
+var elfFemale = ["Adrie", "Althaea", "Anastrianna", "Andraste", "Antinua"];
+var elfSurname = ["Amakiir", "Amastacia", "Galanodel", "Holimion", "Ilphelkiir", "Liadon"];
